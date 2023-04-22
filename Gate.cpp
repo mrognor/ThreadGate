7e239f313c2273a7ce6ed0e9655bc2e0586780e7
Timur Ammaev <timurlego1@yandex.ru>
2023-04-22 23:09:33 +0300

    Fix bugs

--- Gate.cpp
+++ Gate.cpp
@@ -4,21 +4,29 @@
 class Gate
 {
 private:
-    std::mutex mtx, lockMutex;
+    std::mutex mtx, condVarMutex, lockMutex;
     std::unique_lock<std::mutex> lk = std::unique_lock<std::mutex>(lockMutex);
     std::condition_variable cv;
     bool IsActivated = true;
 public:
-    // Blocks the execution of the thread until the Run method is called. 
+    // Blocks the execution of the thread until the Open method is called. 
     // If the Run method was called before this method, then the thread is not blocked.
     // The Close and Open methods are synchronized with each other using a mutex
     void Close()
     {
         mtx.lock();
         if (IsActivated)
+        {
+            condVarMutex.lock();
+            mtx.unlock();
             cv.wait(lk);
+            condVarMutex.unlock();
+            mtx.lock();
+        }
         else
+        {
             IsActivated = true;
+        }
         mtx.unlock();
     }
 
@@ -27,18 +35,22 @@ public:
     // The Close and Open methods are synchronized with each other using a mutex
     void Open()
     {
-        if (mtx.try_lock())
+        mtx.lock();
+        if (condVarMutex.try_lock())
         {
             IsActivated = false;
-            mtx.unlock();
+            condVarMutex.unlock();
         }
         else
         {
-            cv.notify_one();
+            while (condVarMutex.try_lock() != true)
+                cv.notify_all();
+
+            condVarMutex.unlock();
         }
+        mtx.unlock();
     }
 };
-
 
 int main()
 {
@@ -51,13 +63,13 @@ int main()
             g.Close();
         });   
 
-    std::thread th2([&]()
-        {
-            g.Close();
-        });
+    th1.join();
 
+    std::thread th2([&]()
+    {
+        g.Close();
+    });   
     g.Open();
 
-    th1.join();
     th2.join();
 }
