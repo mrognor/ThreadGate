43fe1ba4c3db4277098272ab611c40662e95fd20
mrognor <timurlego1@yandex.ru>
2023-05-03 12:04:57 +0300

    Add time gate class

--- Gate.cpp
+++ Gate.cpp
@@ -15,7 +15,7 @@ private:
     bool IsActivated = true;
 public:
     // Blocks the execution of the thread until the Open method is called. 
-    // If the Run method was called before this method, then the thread is not blocked.
+    // If the Open method was called before this method, then the thread is not blocked.
     // The Close and Open methods are synchronized with each other using a mutex
     void Close()
     {
@@ -35,52 +35,8 @@ public:
         mtx.unlock();
     }
 
-    // Blocks the execution of the thread until the Open method is called or time's not up.
-    // If the Run method was called before this method, then the thread is not blocked.
-    // The Close and Open methods are synchronized with each other using a mutex
-    template<class T>
-    void CloseFor(std::chrono::duration<T> duration)
-    {
-        mtx.lock();
-        if (IsActivated)
-        {
-            condVarMutex.lock();
-            mtx.unlock();
-            cv.wait_for(lk, duration);
-            condVarMutex.unlock();
-            mtx.lock();
-        }
-        else
-        {
-            IsActivated = true;
-        }
-        mtx.unlock();
-    }
-
-    // Blocks the execution of the thread until the Open method is called or the time has come for.
-    // If the Run method was called before this method, then the thread is not blocked.
-    // The Close and Open methods are synchronized with each other using a mutex
-    template<class T>
-    void CloseUntil(std::chrono::time_point<T> timePoint)
-    {
-        mtx.lock();
-        if (IsActivated)
-        {
-            condVarMutex.lock();
-            mtx.unlock();
-            cv.wait_until(lk, timePoint);
-            condVarMutex.unlock();
-            mtx.lock();
-        }
-        else
-        {
-            IsActivated = true;
-        }
-        mtx.unlock();
-    }
-
     // Causes the thread to continue executing after the Close method. 
-    // If called before the Sleep method, then the Sleep method will not block the thread.
+    // If called before the Close method, then the Close method will not block the thread.
     // The Close and Open methods are synchronized with each other using a mutex
     void Open()
     {
@@ -115,7 +71,7 @@ private:
     int ClosingAmount = 0;
 public:
     // Blocks the execution of the thread until the Open method is called. 
-    // If the Run method was called before this method, then the thread is not blocked.
+    // If the Open method was called before this method, then the thread is not blocked.
     // The Close and Open methods are synchronized with each other using a mutex
     void Close()
     {
@@ -132,46 +88,8 @@ public:
         mtx.unlock();
     }
 
-    // Blocks the execution of the thread until the Open method is called or time's not up.
-    // If the Run method was called before this method, then the thread is not blocked.
-    // The Close and Open methods are synchronized with each other using a mutex
-    template<class T>
-    void CloseFor(std::chrono::duration<T> duration)
-    {
-        mtx.lock();
-        if (ClosingAmount <= 0)
-        {
-            condVarMutex.lock();
-            mtx.unlock();
-            cv.wait_for(lk, duration);
-            condVarMutex.unlock();
-            mtx.lock();
-        }
-        --ClosingAmount;
-        mtx.unlock();
-    }
-
-    // Blocks the execution of the thread until the Open method is called or the time has come for.
-    // If the Run method was called before this method, then the thread is not blocked.
-    // The Close and Open methods are synchronized with each other using a mutex
-    template<class T>
-    void CloseUntil(std::chrono::time_point<T> timePoint)
-    {
-        mtx.lock();
-        if (ClosingAmount <= 0)
-        {
-            condVarMutex.lock();
-            mtx.unlock();
-            cv.wait_until(lk, timePoint);
-            condVarMutex.unlock();
-            mtx.lock();
-        }
-        --ClosingAmount;
-        mtx.unlock();
-    }
-
     // Causes the thread to continue executing after the Close method. 
-    // If called before the Sleep method, then the Sleep method will not block the thread.
+    // If called before the Close method, then the Close method will not block the thread.
     // The Close and Open methods are synchronized with each other using a mutex
     void Open()
     {
@@ -187,6 +105,121 @@ public:
             condVarMutex.unlock();
 
         ++ClosingAmount;
+        mtx.unlock();
+    }
+};
+
+// This class implements a gate for a thread. It works as condition variable, 
+// but if the Open method was called in another thread before the Close method, 
+// then the Close method will not block the thread. Doesn't make sense when working in more than two threads
+class TimeGate
+{
+private:
+    std::mutex mtx, condVarMutex, lockMutex;
+    std::unique_lock<std::mutex> lk = std::unique_lock<std::mutex>(lockMutex);
+    std::condition_variable cv;
+    bool IsActivated = true;
+public:
+    // Blocks the execution of the thread until the Open method is called. 
+    // If the Open method was called before this method, then the thread is not blocked.
+    // The Close and Open methods are synchronized with each other using a mutex
+    void Close()
+    {
+        mtx.lock();
+        if (IsActivated)
+        {
+            condVarMutex.lock();
+            mtx.unlock();
+            cv.wait(lk);
+            condVarMutex.unlock();
+            mtx.lock();
+        }
+        else
+        {
+            IsActivated = true;
+        }
+        mtx.unlock();
+    }
+
+    // Blocks the execution of the thread until the Open method is called or time's not up.
+    // If the Open method was called before this method, then the thread is not blocked.
+    // The Close and Open methods are synchronized with each other using a mutex.
+    // After the time expires, the gate remains active, which means that the following Close method will block the thread.
+    template<class T>
+    void CloseFor(std::chrono::duration<T> duration)
+    {
+        mtx.lock();
+        if (IsActivated)
+        {
+            condVarMutex.lock();
+            mtx.unlock();
+            cv.wait_for(lk, duration);
+            condVarMutex.unlock();
+            mtx.lock();
+        }
+        else
+        {
+            IsActivated = true;
+        }
+        mtx.unlock();
+    }
+
+    // Blocks the execution of the thread until the Open method is called or the time has come for.
+    // If the Open method was called before this method, then the thread is not blocked.
+    // The Close and Open methods are synchronized with each other using a mutex.
+    // After the time expires, the gate remains active, which means that the following Close method will block the thread.
+    template<class T>
+    void CloseUntil(std::chrono::time_point<T> timePoint)
+    {
+        mtx.lock();
+        if (IsActivated)
+        {
+            condVarMutex.lock();
+            mtx.unlock();
+            cv.wait_until(lk, timePoint);
+            condVarMutex.unlock();
+            mtx.lock();
+        }
+        else
+        {
+            IsActivated = true;
+        }
+        mtx.unlock();
+    }
+
+    // Causes the thread to continue executing after the Close method. 
+    // If called before the Close method or after CloseFor time expires, then the Close method will not block the thread.
+    // The Close and Open methods are synchronized with each other using a mutex
+    void Open()
+    {
+        mtx.lock();
+        if (condVarMutex.try_lock())
+        {   
+            IsActivated = false;
+            condVarMutex.unlock();
+        }
+        else
+        {
+            while (condVarMutex.try_lock() != true)
+                cv.notify_all();
+
+            condVarMutex.unlock();
+        }
+        mtx.unlock();
+    }
+
+    // Causes the thread to continue executing after the CloseFor method. If the time is over before this method is called, then it will not do anything
+    // If called before the Close method, then the Close method will block the thread.
+    // The Close and Open methods are synchronized with each other using a mutex
+    void OpenIfClosed()
+    {
+        mtx.lock();
+
+        while (condVarMutex.try_lock() != true)
+            cv.notify_all();
+
+        condVarMutex.unlock();
+        
         mtx.unlock();
     }
 };
@@ -228,19 +261,14 @@ int main()
 
     std::cout << "Recursive gates" << std::endl;
     
-    g.CloseFor(std::chrono::seconds(4));
+    TimeGate tg;
 
-    std::cout << "Regular time gate for" << std::endl;
-
-    rg.CloseFor(std::chrono::seconds(4));
-
-    std::cout << "Recursive time gate for" << std::endl;
+    tg.CloseFor(std::chrono::seconds(4));
     
+    std::cout << "Time gate for" << std::endl;
+
     auto now = std::chrono::system_clock::now();
 
-    g.CloseUntil(now + std::chrono::seconds(5));
-    std::cout << "Regular time gate until" << std::endl;
-    
-    rg.CloseUntil(now + std::chrono::seconds(5));
-    std::cout << "Recursive time gate until" << std::endl;  
+    tg.CloseUntil(now + std::chrono::seconds(5));
+    std::cout << "Time gate until" << std::endl;
 }
